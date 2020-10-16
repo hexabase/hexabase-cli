2234140e2ec1e9ac82c7abb9acdb9715b3f0868f
Fritz Lin <y.lin@b-eee.com>
2020-10-16 17:49:49 +0900

    Add interactivity to init cmd

--- src/commands/init/index.ts
+++ src/commands/init/index.ts
@@ -1,13 +1,54 @@
 import {Command} from '@oclif/command'
+import {readdirSync} from 'fs'
+import {prompt}  from 'enquirer'
+import cli from 'cli-ux'
 import * as download from 'download'
+import * as chalk from 'chalk'
+
+interface UrlMap {
+  [key: string]: string;
+}
+
+const urlMap: UrlMap = {
+  vue: 'https://github.com/b-eee/hexa-vue-example1/archive/develop.zip',
+}
+
+const questions = [
+  {
+    type: 'select',
+    name: 'template',
+    message: 'Select a template',
+    choices: ['vue'],
+  },
+  {
+    type: 'input',
+    name: 'name',
+    message: `Specify the ${chalk.cyan('name')} of your app`,
+    validate: function (input: string) {
+      if (input.length === 0) {
+        return 'Cannot be empty'
+      }
+      return input.length !== 0
+    },
+  },
+]
 
 export default class Init extends Command {
   static description = 'initialize a new app'
 
   async run() {
     try {
-      const url = 'https://github.com/b-eee/hexa-vue-example1/archive/develop.zip'
-      const dest = './vue-app'
+      const answers: Answer = await prompt(questions)
+      const {template, name} = answers
+
+      const isExistsDir = readdirSync('.', {withFileTypes: true})
+      .some(dirent => dirent.isDirectory() && dirent.name === name)
+      if (isExistsDir) {
+        throw new Error('Folder with same name already exists')
+      }
+
+      cli.action.start(`initializing app with name ${chalk.cyan(name)}`)
+      const url = urlMap[template]
       const downloadOptions = {
         extract: true,
         strip: 1,
@@ -16,9 +57,11 @@ export default class Init extends Command {
           accept: 'application/zip',
         },
       }
-      await download(url, dest, downloadOptions)
+      await download(url, name, downloadOptions)
     } catch (error) {
       this.error(error)
+    } finally {
+      cli.action.stop()
     }
   }
 }
---
+++ src/global.d.ts
@@ -0,0 +1,3 @@
+interface Answer {
+  [key: string]: string;
+}
