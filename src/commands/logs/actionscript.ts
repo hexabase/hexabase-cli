def7e4f997e7d6796a15a93fdc9cd2dbb86d5a84
Fritz Lin <y.lin@b-eee.com>
2020-10-21 20:07:27 +0900

    Get sse url from context config

--- src/commands/logs/actionscript.ts
+++ src/commands/logs/actionscript.ts
@@ -1,5 +1,8 @@
 import {Command, flags} from '@oclif/command'
 import EventSource from 'eventsource'
+import Conf from 'conf'
+
+const config = new Conf()
 
 export default class Logs extends Command {
   static description = 'get logs from actionscript'
@@ -14,7 +17,12 @@ export default class Logs extends Command {
     const {args} = this.parse(Logs)
     const {channel} = args
 
-    const url = `http://localhost:5002/sse?channel=${channel}`
+    const currentContext = config.get('current-context')
+    const sseUrl = config.get(`contexts.${currentContext}.sse`)
+
+    // TODO: empty currentContext, sseUrl
+
+    const url = `${sseUrl}/sse?channel=${channel}`
     const source = new EventSource(url)
     this.log('Listening for logs...')
     source.addEventListener('log_actionscript', (event: Event) => {
