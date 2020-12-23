f2bc2d752ce54e281408dcaf88bb2d00d12134f3
Fritz Lin <y.lin@b-eee.com>
2020-12-23 16:57:44 +0900

    Add confirmation prompt to projects:restore

--- src/commands/projects/restore.ts
+++ src/commands/projects/restore.ts
@@ -4,6 +4,7 @@ import cli from 'cli-ux'
 import chalk from 'chalk'
 import BaseWithContext from '../../base-with-context'
 import * as tmp from '../../api/projects/templates'
+import * as ws from '../../api/workspaces/workspaces'
 
 const questions = [
   {
@@ -17,6 +18,11 @@ const questions = [
       return input.length !== 0
     },
   },
+  {
+    type: 'confirm',
+    name: 'confirm',
+    message: 'Do you want to proceed?',
+  },
 ]
 
 export default class ProjectsRestore extends BaseWithContext {
@@ -26,6 +32,7 @@ export default class ProjectsRestore extends BaseWithContext {
     ...BaseWithContext.flags,
     help: flags.help({char: 'h'}),
     name: flags.string({char: 'n', description: 'name of the project to be restored'}),
+    yes: flags.boolean({char: 'y', description: 'skip confirmation'}),
   }
 
   static args = [
@@ -45,9 +52,28 @@ export default class ProjectsRestore extends BaseWithContext {
         flags.name = await prompt(questions[0]).then(({pj_name}: any) => pj_name)
       }
 
-      cli.action.start(`restoring template from file ${chalk.cyan(args.file)}`)
-      await tmp.uploadTemplate(this.currentContext, flags.name!, args.file,)
-      cli.action.stop()
+      const workspaceResponse = await ws.get(this.currentContext)
+      const currentWorkspace = workspaceResponse.workspaces.find((ws): boolean => {
+        return ws.workspace_id === workspaceResponse.current_workspace_id
+      })
+
+      let shouldProceed = false
+      if (flags.yes) {
+        shouldProceed = true
+      } else {
+        this.log(`You are about to restore the template to:
+  workspace: ${chalk.cyan(currentWorkspace?.workspace_name)}
+  context: ${chalk.cyan(this.currentContext)}`)
+        shouldProceed = await prompt(questions[1]).then(({confirm}: any) => confirm as boolean)
+      }
+
+      if (shouldProceed) {
+        cli.action.start(`restoring template from file ${chalk.cyan(args.file)}`)
+        // await tmp.uploadTemplate(this.currentContext, flags.name!, args.file)
+        cli.action.stop()
+      } else {
+        this.log(chalk.red('restoring  aborted'))
+      }
     } finally {
       if (noNameFlag) {
         this.log(
