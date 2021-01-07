8f627c8fc80f8f56c781d4b96d523a67b47197a6
Fritz Lin <y.lin@b-eee.com>
2021-01-07 11:11:05 +0900

    Simple test for workspaces:get

--- test/commands/workspaces/get.test.ts
+++ test/commands/workspaces/get.test.ts
@@ -2,9 +2,22 @@ import {expect, test} from '@oclif/test'
 
 describe('workspaces:get', () => {
   test
+  .nock(
+    'http://localhost:7575',
+    api =>
+      api
+      .get('/api/v0/workspaces')
+      .reply(200, {
+        workspaces: [{
+          workspace_id: '12345',
+          workspace_name: 'test',
+        }],
+        current_workspace_id: '12345',
+      })
+  )
   .stdout()
-  .command(['workspaces:get'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
+  .command(['workspaces:get', '-c', 'local'])
+  .it('runs', ctx => {
+    expect(ctx.stdout).to.contain('ID                            NAME \n12345                         test \nCurrent-workspace set to: test\n')
   })
 })
