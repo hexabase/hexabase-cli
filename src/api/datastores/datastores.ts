b11be9030cae36e59a3583ed7619b9d419e44bcf
Fritz Lin <y.lin@b-eee.com>
2021-01-08 11:47:37 +0900

    Add datastores:get cmd

---
+++ src/api/datastores/datastores.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import Conf from 'conf'
+import {Context} from '../../base-with-context'
+
+interface GetDatastoresElemResponse {
+  datastore_id: string;
+  name: string;
+  display_id: string;
+  deleted: boolean;
+}
+
+const config = new Conf()
+
+export const get = async (currentContext: string, p_id: string): Promise<GetDatastoresElemResponse[]> => {
+  try {
+    const context = config.get(`contexts.${currentContext}`) as Context
+    const url = `${context.server}/api/v0/applications/${p_id}/datastores`
+    const token = config.get(`hexabase.${currentContext}.token`)
+    const requestConfig = {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    }
+    const {data}: {data: GetDatastoresElemResponse[]} = await axios.get(url, requestConfig)
+    return data
+  } catch (error) {
+    throw error
+  }
+}
---
+++ src/commands/datastores/get.ts
@@ -0,0 +1,77 @@
+import {flags} from '@oclif/command'
+import {prompt}  from 'enquirer'
+import {cli} from 'cli-ux'
+import * as ws from '../../api/workspaces/workspaces'
+import * as pj from '../../api/projects/projects'
+import * as ds from '../../api/datastores/datastores'
+import BaseWithContext from '../../base-with-context'
+
+const questions = [
+  {
+    type: 'select',
+    name: 'project',
+    message: 'Select your project',
+    choices: [],
+  },
+]
+
+export default class DatastoresGet extends BaseWithContext {
+  static description = 'get datastores within a project'
+
+  static flags = {
+    ...BaseWithContext.flags,
+    help: flags.help({char: 'h'}),
+    ...cli.table.flags(),
+  }
+
+  static args = [
+    {
+      name: 'projectId',
+      description: 'projectId from hexabase',
+    },
+  ]
+
+  async run() {
+    const {args, flags} = this.parse(DatastoresGet)
+
+    if (!args.projectId) {
+      const currentWorkspace = await ws.current(this.currentContext)
+      const projects = await pj.get(this.currentContext, currentWorkspace.workspace_id)
+      questions[0].choices = projects.map(pj => {
+        return {
+          name: pj.application_id,
+          message: pj.name,
+          hint: pj.application_id,
+        }
+      }) as never[]
+      const {project: project_id}: {project: string} = await prompt(questions[0])
+      args.projectId = project_id
+    }
+
+    const datastores = await ds.get(this.currentContext, args.projectId)
+
+    const columns = {
+      datastore_id: {
+        header: 'ID',
+        minWidth: 30,
+      },
+      display_id: {
+        header: 'DISPLAY_ID',
+        minWidth: 20,
+      },
+      name: {
+        header: 'NAME',
+        minWidth: 20,
+      },
+      deleted: {
+        header: 'DELETED',
+        extended: true,
+      },
+    }
+
+    cli.table(datastores, columns, {
+      printLine: this.log,
+      ...flags,
+    })
+  }
+}
