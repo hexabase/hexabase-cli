64c6a1624be5410e581480a3da37efd023446245
Fritz Lin <y.lin@b-eee.com>
2021-01-25 14:42:02 +0900

    Use APIClient for actions:get

--- src/api/actions/actions.ts
+++ src/api/actions/actions.ts
@@ -74,26 +74,6 @@ export const create = async (currentContext: string, d_id: string, data: ActionD
   }
 }
 
-export const get = async (currentContext: string, d_id: string, s_id = ''): Promise<GetActionsElemResponse[]> => {
-  try {
-    const context = config.get(`contexts.${currentContext}`) as Context
-    let url = `${context.server}/api/v0/datastores/${d_id}/actions`
-    if (s_id) {
-      url = `${url}?status_id=${s_id}`
-    }
-    const token = config.get(`hexabase.${currentContext}.token`)
-    const requestConfig = {
-      headers: {
-        authorization: `Bearer ${token}`,
-      },
-    }
-    const {data}: {data: GetActionsElemResponse[]} = await axios.get(url, requestConfig)
-    return data
-  } catch (error) {
-    throw error
-  }
-}
-
 export const getOne = async (currentContext: string, d_id: string, a_id: string): Promise<GetActionSettingsResponse> => {
   try {
     const context = config.get(`contexts.${currentContext}`) as Context
--- src/commands/actions/get.ts
+++ src/commands/actions/get.ts
@@ -1,7 +1,7 @@
 import {flags} from '@oclif/command'
 import {cli} from 'cli-ux'
-import * as actn from '../../api/actions/actions'
 import BaseWithContext from '../../base-with-context'
+import {GetActionsElemResponse} from '../../api/actions/actions'
 
 export default class ActionsGet extends BaseWithContext {
   static description = 'get actions of a datastore'
@@ -27,7 +27,11 @@ export default class ActionsGet extends BaseWithContext {
   async run() {
     const {args, flags} = this.parse(ActionsGet)
 
-    const actions = await actn.get(this.currentContext, args.datastore_id, args.status_id)
+    let url = `/api/v0/datastores/${args.datastore_id}/actions`
+    if (args.status_id) {
+      url = `${url}?status_id=${args.status_id}`
+    }
+    const {data: actions} = await this.hexaapi.get<GetActionsElemResponse[]>(url)
 
     const columns = {
       action_id: {
@@ -49,14 +53,14 @@ export default class ActionsGet extends BaseWithContext {
       set_status: {
         header: 'SET_STATUS_ID',
         extended: true,
-        get: (row: actn.GetActionsElemResponse) => {
+        get: (row: GetActionsElemResponse) => {
           return row.set_status ? row.set_status : ''
         },
       },
       status_id: {
         header: 'STATUS_ID',
         extended: true,
-        get: (row: actn.GetActionsElemResponse) => {
+        get: (row: GetActionsElemResponse) => {
           return row.status_id ? row.status_id : ''
         },
       },
