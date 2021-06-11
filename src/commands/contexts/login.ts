c77ee02db2633566c76f74791b5d35617f0ac1dd
iwask <h.iwasaki@b-eee.com>
2021-06-11 22:30:58 +0900

    update login

--- src/commands/contexts/login.ts
+++ src/commands/contexts/login.ts
@@ -25,20 +25,32 @@ export default class ContextsLogin extends BaseWithContext {
   static flags = {
     ...BaseWithContext.flags,
     help: flags.help({char: 'h'}),
+    email: flags.string({char: 'u', description: 'user email address to login'}),
+    password: flags.string({char: 'p', description: 'login password'}),
   }
 
   async run() {
-    this.parse(ContextsLogin)
+    const {args, flags} = this.parse(ContextsLogin)
+    let {email, password} = flags
 
-    const {email}: {email: string} = await prompt(this.questions[0])
-    const {password}: {password: string} = await prompt(this.questions[1])
+    if (!flags.email) {
+      email = await prompt(this.questions[0])
+    }
+    if (!flags.password) {
+      const p:{password: string} = await prompt(this.questions[1])
+      password = p.password
+    }
 
     const data = {email, password}
     const url = '/api/v0/login'
-    const {data: {token}} = await this.hexaapi.post<PostLoginResponse>(url, data)
+    try{
+      const {data: {token}} = await this.hexaapi.post<PostLoginResponse>(url, data)
+      this.hexaconfig.set(`hexabase.${this.currentContext}.email`, email)
+      this.hexaconfig.set(`hexabase.${this.currentContext}.token`, token)
+      this.log(`Successfully logged in as: ${chalk.cyan(email)}`)
+    }catch{
+      this.log(`login ${chalk.red('failed')} for ${chalk.cyan(email)}`)
+    }
 
-    this.hexaconfig.set(`hexabase.${this.currentContext}.email`, email)
-    this.hexaconfig.set(`hexabase.${this.currentContext}.token`, token)
-    this.log(`Successfully logged in as: ${chalk.cyan(email)}`)
   }
 }
