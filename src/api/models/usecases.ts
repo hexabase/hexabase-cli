e5283fd0fea575f02bd95956697510831782c10d
H.Iwasaki <h.iwasaki@hexabase.com>
2024-06-12 15:23:50 +0900

    update usecase client

--- src/api/models/usecases.ts
+++ src/api/models/usecases.ts
@@ -1,12 +1,46 @@
+export interface ActorResponse {
+  id: string;
+  name: string;
+  type: string;
+  description: string;
+}
+
+export interface UserInterfaceResponse {
+  id: string;
+  name: string;
+  type: string;
+  description: string;
+}
+
+export interface SystemInterfaceResponse {
+  id: string;
+  name: string;
+  type: string;
+  description: string;
+}
+
 export interface UseCaseResponse {
   id: string;
   title: string;
   description: string;
   actors: string[];
+  user_interfaces: string[];
+  system_interfaces: string[];
+}
+
+export interface GenerateUseCasesResult {
+  name: string;
+  goal: string;
+  description: string;
+  actors: ActorResponse[];
+  user_interfaces: UserInterfaceResponse[];
+  system_interfaces: SystemInterfaceResponse[];
+  usecases: UseCaseResponse[];
 }
 
 export interface GenerateUseCasesResponse {
-  result: UseCaseResponse[];
+  result: GenerateUseCasesResult;
+  total_tokens: number;
 }
 
 
--- src/commands/usecases/generate.ts
+++ src/commands/usecases/generate.ts
@@ -9,7 +9,7 @@ export default class UseCasesGenerate extends BaseWithContext {
   private questions = [
     {
       type: 'input',
-      name: 'requrirement',
+      name: 'requirement',
       message: `Input the ${chalk.cyan('requirement')} for your app`,
       validate: function (input: string) {
         if (input.length === 0) {
@@ -22,7 +22,7 @@ export default class UseCasesGenerate extends BaseWithContext {
       type: 'select',
       name: 'examples',
       message: 'Select a template',
-      choices: ['example1............', 'example2..................'],
+      choices: ['Generate Job Matching SaaS Application', 'Generate Web site with Information Board and Blog Application'],
     },
   ]
 
@@ -39,7 +39,7 @@ export default class UseCasesGenerate extends BaseWithContext {
 
   static args = [
     {
-      name: 'query',
+      name: 'prompt',
       description: 'Input the requirement for your app',
     },
   ]
@@ -53,32 +53,111 @@ export default class UseCasesGenerate extends BaseWithContext {
     let queryString = flags.input
     let url = '/api/v0/usecases/generate' 
     let postData = {
-      query: queryString,
+      prompt: queryString,
     }
 
     cli.action.start(`Generating UseCases. This may take a while`)
     const {data: usecasesResponse} = await this.hexaModeler.post<GenerateUseCasesResponse>(url, postData)
 
+  
+    // Display the Results
+    this.log(`---- SYSTEM NAME ----`);
+    this.log(`${usecasesResponse.result.name}`);
+    this.log(`---- GOAL ----`);
+    this.log(`${usecasesResponse.result.goal}`);
+    this.log(`---- DESCRIPTION ----`);
+    this.log(`${usecasesResponse.result.description}`);
+    
+    this.log(`---- ACTORS ----`);
+    const actorsColumns = {
+      id: {
+        header: 'Actor ID',
+      },
+      name: {
+        header: 'Actor Name',
+      },
+      type: {
+        header: 'Type',
+      },
+      description: {
+        header: 'Description',
+      },
+    }
+    cli.table(usecasesResponse.result.actors, actorsColumns, {
+      printLine: this.log,
+      ...flags,
+    })
+    
+    this.log(`---- USER INTERFACES(SCREENS) ----`);
+    const userInterfaceColumns = {
+      id: {
+        header: 'ID',
+      },
+      name: {
+        header: 'UI Name',
+      },
+      type: {
+        header: 'Type',
+      },
+      description: {
+        header: 'Description',
+      },
+    }
+    cli.table(usecasesResponse.result.user_interfaces, userInterfaceColumns, {
+      printLine: this.log,
+      ...flags,
+    })
+
+
+    this.log(`---- SYSTEM INTERFACES to External systems ----`);
+    const systemInterfaceColumns = {
+      id: {
+        header: 'ID',
+      },
+      name: {
+        header: 'IF Name',
+      },
+      type: {
+        header: 'Type',
+      },
+      description: {
+        header: 'Description',
+      },
+    }
+    cli.table(usecasesResponse.result.system_interfaces, systemInterfaceColumns, {
+      printLine: this.log,
+      ...flags,
+    })
+
+    // Display the Usecases Results
+    this.log(`---- USE CASES ----`);
     const columns = {
       id: {
         header: 'ID',
       },
       title: {
-        header: 'TITLE',
+        header: 'Title',
       },
       actors: {
-        header: 'ACTORS',
+        header: 'Actors',
+      },
+      user_interfaces: {
+        header: 'User Interfaces',
+      },
+      system_interfaces: {
+        header: 'System Interfaces',
       },
       description: {
-        header: 'DESCRIPTION',
+        header: 'Description',
       },
     }
-    cli.table(usecasesResponse.result, columns, {
+    cli.table(usecasesResponse.result.usecases, columns, {
       printLine: this.log,
       ...flags,
     })
+
     cli.action.stop()
-    this.log(`Generated: ${usecasesResponse.result.length} UseCases`)
+    this.log(`Generated: ${usecasesResponse.result.usecases.length} UseCases. Total Tokens: ${usecasesResponse.total_tokens}`)
 
   }
 }
